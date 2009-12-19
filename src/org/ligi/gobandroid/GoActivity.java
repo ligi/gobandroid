83276762dbd445883653bbaac4d292e73fb25323
marcus bueschleb <ligi@ligi.de>
2009-12-19 01:05:59 +0100

    rm log stuff

--- src/org/ligi/gobandroid/GoActivity.java
+++ src/org/ligi/gobandroid/GoActivity.java
@@ -23,18 +23,12 @@ public class GoActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("gobandroid" , "before setsize");
         Intent getIntent=getIntent(); 
-        byte size=getIntent.getByteExtra("size",(byte)13);
-        Log.d("gobandroid" , "size"+size);
-        
-        
-        
+        byte size=getIntent.getByteExtra("size",(byte)9);
         GoGame game=new GoGame(size);
         View board_view=new GoBoardView(this,game);
         board_view.setOnTouchListener((OnTouchListener)board_view );
         setContentView(board_view);
-        
     }
       
 }
