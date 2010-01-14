af1d60ff3a87fddafb6eea899030e10a7ac9ccc5
marcus bueschleb <ligi@ligi.de>
2010-01-14 02:48:22 +0100

    initial settings activity

---
+++ src/org/ligi/gobandroid/SettingsActivity.java
@@ -0,0 +1,39 @@
+package org.ligi.gobandroid;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.EditText;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Activity for a Game
+ * 
+ * @author <a href="http://ligi.de">Marcus -Ligi- Bueschleb</a>
+ * 
+ *         This software is licenced with GPLv3
+ * 
+ **/
+
+public class SettingsActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.settings);
+	}
+
+}
