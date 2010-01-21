f74525602b5c660f8a79a9e23fae9899a7705da2
marcus bueschleb <ligi@ligi.de>
2010-01-21 22:51:41 +0100

    remove unused imports

--- src/org/ligi/gobandroid/ui/SettingsActivity.java
+++ src/org/ligi/gobandroid/ui/SettingsActivity.java
@@ -1,25 +1,8 @@
 package org.ligi.gobandroid.ui;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnTouchListener;
-import android.widget.EditText;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
 import org.ligi.gobandroid.R;
-import org.ligi.gobandroid.R.layout;
 
 /**
  * Activity for a Game
