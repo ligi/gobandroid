12e0e1b5efa593509c4df9b95fad8bb7c58ca3f7
marcus bueschleb <ligi@ligi.de>
2010-01-21 22:50:06 +0100

    initial SGFWriter

---
+++ src/org/ligi/gobandroid/logic/SGFWriter.java
@@ -0,0 +1,26 @@
+package org.ligi.gobandroid.logic;
+
+public class SGFWriter {
+
+	public static String game2sgf(GoGame game) {
+		String res="";
+		res="(;FF[4]GM[1]"; // header
+		res+="SZ[" + game.getBoardSize() + "]"; // board_size;
+		res+="\n";
+		
+		boolean black_to_move=true;
+		
+		for (int move=0;move<game.moves.size();move++)
+			{
+			byte[] act_move=game.moves.get(move);
+			res+=";" + (black_to_move?"B":"W") + "[" + (char)('a'+act_move[0]) +(char)('a'+act_move[1])+ "]\n";
+			black_to_move=!black_to_move;
+			}
+			
+		res+=")"; // close
+		return res;
+	}
+	
+	
+	
+}
