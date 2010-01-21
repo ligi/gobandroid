1c7d690239db71a5453b5d4e3e7c74c02c157210
marcus bueschleb <ligi@ligi.de>
2010-01-21 22:51:16 +0100

    remove unused imports

--- src/org/ligi/gobandroid/ui/GoBoardView.java
+++ src/org/ligi/gobandroid/ui/GoBoardView.java
@@ -6,8 +6,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
