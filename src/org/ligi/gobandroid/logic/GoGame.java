3818ad2938ca202191e7976382f5c5f87d54b9f8
marcus bueschleb <ligi@ligi.de>
2010-01-27 17:02:34 +0100

    implement counting

--- src/org/ligi/gobandroid/logic/GoGame.java
+++ src/org/ligi/gobandroid/logic/GoGame.java
@@ -31,15 +31,37 @@ public class GoGame implements GoDefinitions {
     private boolean game_finished=false;
     
     private int[][] groups; // array to build groups
-    private boolean[][] dead_stones; // dead stone marker
+        
+    public int[][] area_groups; // array to build groups
+    public byte[][] area_assign; // cache to which player a area belongs in a finished game
+    
+    
+    //private boolean[][] dead_stones; // dead stone marker
         
     private int group_count = -1;
         
     private int captures_white; // counter for the captures from black
     private int captures_black; // counter for the captures from white
     
+    
+    public int territory_white; // counter for the captures from black
+    public int territory_black; // counter for the captures from white
+    
     private int handicap=0;
     
+    float komi=6.5f;
+    
+    public float getKomi() {
+    	return komi;
+    }
+
+    public float getPointsWhite() {
+    	return komi+captures_white+territory_white;
+    }
+    
+    public float getPointsBlack() {
+    	return captures_black+territory_black;
+    }
     public Vector<byte[]> moves;
 
     public GoGame( byte size ) {
@@ -74,12 +96,16 @@ public class GoGame implements GoDefinitions {
         
         // create the array for group calculations
         groups = new int[size][size];
-        dead_stones=new boolean[size][size];
         
-        for (int x=0;x<size;x++)
+        area_groups = new int[size][size];
+        area_assign = new byte[size][size];
+                
+        //dead_stones=new boolean[size][size];
+        
+        /*for (int x=0;x<size;x++)
         	for (int y=0;y<size;y++)
         		dead_stones[x][y]=false;
-        	
+        	*/
         reset();	
         
         
@@ -97,18 +123,20 @@ public class GoGame implements GoDefinitions {
     public void pass() {
         if (last_action_was_pass) {   	// finish game if both passed  
             game_finished=true; 
-        	build_groups();	}
+        	buildGroups();	
+        	buildAreaGroups();	
+        }
         else {
             last_action_was_pass=true;
             moves.add(new byte[] { -1,-1} );
             setNextPlayer();
         }
     }
-
+/*
     public boolean isStoneDead(byte x,byte y) {
     	return dead_stones[x][y];
     }
-    
+*/  
     /**
      *  place a stone on the board
      *
@@ -124,7 +152,9 @@ public class GoGame implements GoDefinitions {
         		for (int xg = 0; xg < calc_board.getSize(); xg++)
                     for (int yg = 0; yg < calc_board.getSize(); yg++)
                         if (groups[xg][yg]==groups[x][y])
-                        	dead_stones[xg][yg]=!dead_stones[xg][yg];
+                        	calc_board.toggleCellDead(xg, yg);
+        		buildAreaGroups();
+        		
         	}
         	else {
         	
@@ -137,10 +167,10 @@ public class GoGame implements GoDefinitions {
                 else
                     calc_board.setCellWhite( x, y );
                 
-                build_groups();
+                buildGroups();
                 remove_dead(x,y);
                 
-                if ((group_has_liberty(groups[x][y])||isDeadGroupOnBoard(x,y)) // if either a field has libertys or get's one
+                if ((hasGroupLibertie(groups[x][y])||isDeadGroupOnBoard(x,y)) // if either a field has libertys or get's one
                 		&&!pre_last_board.equals(calc_board)) // and the move is not a ko 
                 { 	// valid move -> do things needed to do after a valid move 
                     Log.d("gobandroid", "isDeadGroupOnBoard(x,y)" + isDeadGroupOnBoard(x,y));
@@ -180,7 +210,7 @@ public class GoGame implements GoDefinitions {
                 
         setNextPlayer();
         
-        build_groups();
+        buildGroups();
         remove_dead(x,y); 
         moves.add(new byte[] { x,y} );
     }
@@ -201,9 +231,8 @@ public class GoGame implements GoDefinitions {
         clear_calc_board();
         
        	Vector<byte[]> _moves=  (Vector<byte[]>)moves.clone();
-        Log.i("gobandroid ","after cast");
-        reset();
-        Log.i("gobandroid ","after reset");
+       	reset();
+        
         for (int step=0 ; step<_moves.size()-1;step++)
         {
             byte move_x=_moves.get(step)[0];
@@ -215,33 +244,90 @@ public class GoGame implements GoDefinitions {
         }
         
         visual_board=calc_board.clone();
+    }
+   
+    
+    public boolean cell_has_liberty(int x , int y )    {
+      
+      return ( ((x != 0)&&(calc_board.isCellFree( x- 1, y ) ))
+          ||
+       ((y != 0)&&(calc_board.isCellFree( x, y - 1 )))
+          ||  
+       ((x != (calc_board.getSize() - 1))&& (calc_board.isCellFree( x + 1, y )))
+          ||  
+       ((y != (calc_board.getSize() - 1))&& (calc_board.isCellFree( x, y + 1 ) ))
+      );  
+    }
+   
+
+    public boolean cell_has_white_neighbours(int x , int y )    {
+      
+      return ( ((x != 0)&&(calc_board.isCellWhite( x- 1, y ) ))
+          ||
+       ((y != 0)&&(calc_board.isCellWhite( x, y - 1 )))
+          ||  
+       ((x != (calc_board.getSize() - 1))&& (calc_board.isCellWhite( x + 1, y )))
+          ||  
+       ((y != (calc_board.getSize() - 1))&& (calc_board.isCellWhite( x, y + 1 ) ))
+      );  
+    }
+   
+    public boolean cell_has_black_neighbours(int x , int y )    {
         
-    }
-   
-    public boolean cell_has_liberty(int x , int y )
-    {
-      
-      if ((x != 0)&&(calc_board.isCellFree( x- 1, y ) ))
-          return true;
-      if ((y != 0)&&(calc_board.isCellFree( x, y - 1 )))
-          return true;  
-      if ((x != (calc_board.getSize() - 1))&& (calc_board.isCellFree( x + 1, y )))
-          return true;  
-      if ((y != (calc_board.getSize() - 1))&& (calc_board.isCellFree( x, y + 1 ) ))
-          return true;  
-      
-      return false;
-    }
-   
-    public boolean group_has_liberty(int group2check ) {
-        for (int xg = 0; xg < calc_board.getSize(); xg++)
-            for (int yg = 0; yg < calc_board.getSize(); yg++)
+        return ( ((x != 0)&&(calc_board.isCellBlack( x- 1, y ) ))
+            ||
+         ((y != 0)&&(calc_board.isCellBlack( x, y - 1 )))
+            ||  
+         ((x != (calc_board.getSize() - 1))&& (calc_board.isCellBlack( x + 1, y )))
+            ||  
+         ((y != (calc_board.getSize() - 1))&& (calc_board.isCellBlack( x, y + 1 ) ))
+        );  
+      }
+    
+    
+    /**
+     * check if a group has liberties
+     * 
+     * @param group2check - the group to check
+     * @return boolean weather the group has a liberty
+     * 
+     */
+    public boolean hasGroupLibertie(int group2check ) {
+        for (int xg = 0; xg < getBoardSize(); xg++)
+            for (int yg = 0; yg < getBoardSize(); yg++)
                 if ((groups[xg][yg]==group2check)&&(cell_has_liberty(xg,yg)))
-                     return true;
+                     return true; // if one of the stones in the group has a liberty -> return true because then the group has a liberty
         return false;  // found no stone in the group with a liberty 
     }
-    
-    
+        
+    public boolean isAreaGroupBlacks(int group2check ) {
+    	if(group2check==-1) return false;
+    	boolean res=false;
+        for (int xg = 0; xg < getBoardSize(); xg++)
+            for (int yg = 0; yg < getBoardSize(); yg++)
+               if (area_groups[xg][yg]==group2check)
+            	   if (cell_has_white_neighbours(xg,yg))
+            		   return false;
+            	   else
+            		   res|=(cell_has_black_neighbours(xg,yg));
+            		   
+        return res;  // found no stone in the group with a liberty 
+    }
+   
+    public boolean isAreaGroupWhites(int group2check ) {
+    	if(group2check==-1) return false;
+    	boolean res=false;
+        for (int xg = 0; xg < getBoardSize(); xg++)
+            for (int yg = 0; yg < getBoardSize(); yg++)
+               if (area_groups[xg][yg]==group2check)
+            	   if (cell_has_black_neighbours(xg,yg))
+            		   return false;
+            	   else
+            		   res|=(cell_has_white_neighbours(xg,yg));
+            		   
+        return res;  // found no stone in the group with a liberty 
+    }
+       
     public void clear_calc_board() {
         for (byte x = 0; x < calc_board.getSize(); x++)
             for (byte y = 0; y < calc_board.getSize(); y++) 
@@ -256,7 +342,7 @@ public class GoGame implements GoDefinitions {
      * 
      */
     
-    public void build_groups() {
+    public void buildGroups() {
         group_count=0;
         
         
@@ -298,6 +384,69 @@ public class GoGame implements GoDefinitions {
             }
 
     }
+
+    
+    int area_group_count=0;
+    
+    public void buildAreaGroups() {
+        area_group_count=0;
+                
+        // reset groups
+        for (int x = 0; x < calc_board.getSize(); x++)
+            for (int y = 0; y < calc_board.getSize(); y++) {
+            	   area_groups[x][y] = -1;
+            	   area_assign[x][y] = 0;
+            }
+        
+        for (byte x = 0; x < calc_board.getSize(); x++)
+            for (byte y = 0; y < calc_board.getSize(); y++) {
+                if (calc_board.isCellFree( x, y )) {
+
+                    if (x > 0) {
+                        if (!calc_board.areCellsEqual( x, y, (byte)( x - 1), y )) {
+                        	area_group_count++;
+                        	area_groups[x][y] = area_group_count;
+                        }
+                        else
+                        	area_groups[x][y] = area_groups[x - 1][y];
+                    }
+                    else {
+                    	area_group_count++;
+                    	area_groups[x][y] = area_group_count;
+                    }
+                    
+                    if (y > 0) {
+                        if (calc_board.areCellsEqual( x, y, x , (byte)(y-1) )) {
+                            int from_grp=area_groups[x][y];
+                            
+                            for (int xg = 0; xg < calc_board.getSize(); xg++)
+                                for (int yg = 0; yg < calc_board.getSize(); yg++)
+                                    if (area_groups[xg][yg]==from_grp)
+                                    	area_groups[xg][yg]=area_groups[x][y-1];
+                        }
+                    }
+
+                }
+            }
+
+        territory_black=0;
+        territory_white=0;
+        for (int x = 0; x < calc_board.getSize(); x++)
+            for (int y = 0; y < calc_board.getSize(); y++) {
+            	if (isAreaGroupWhites(area_groups[x][y]))
+        			{ 
+            		area_assign[x][y]=PLAYER_WHITE;
+            		territory_white++;
+        			}
+            	else if (isAreaGroupBlacks(area_groups[x][y])) {
+            		territory_black++;
+            		area_assign[x][y]=PLAYER_BLACK;
+            	}
+        			
+            }
+        
+    }
+
     
     /**
      * 
@@ -416,7 +565,11 @@ public class GoGame implements GoDefinitions {
     public GoBoard getVisualBoard() {
         return visual_board;
     }
+    public GoBoard getCalcBoard() {
+        return calc_board;
+    }
 
+    
     public boolean isLastActionPass() {
     	return last_action_was_pass;
     }
