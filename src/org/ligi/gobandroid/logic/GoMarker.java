6cd7b304b82dd2306d3c2efcdcd414f9db435d30
marcus bueschleb <ligi@ligi.de>
2010-04-27 02:21:31 +0200

    the GoMarker class

---
+++ src/org/ligi/gobandroid/logic/GoMarker.java
@@ -0,0 +1,44 @@
+/**
+ * gobandroid 
+ * by Marcus -Ligi- Bueschleb 
+ * http://ligi.de
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as 
+ * published by the Free Software Foundation; 
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ **/
+
+package org.ligi.gobandroid.logic;
+
+public class GoMarker {
+
+	private byte x,y;
+	private String text;
+	
+	public GoMarker(byte x,byte y , String text) {
+		this.x=x;
+		this.y=y;
+		this.text=text;
+	}
+	public byte getX() {
+		return x;
+	}
+
+	public byte getY() {
+		return y;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+}
