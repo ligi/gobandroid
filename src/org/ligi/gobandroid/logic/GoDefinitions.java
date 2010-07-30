2d1af55ebce8736388038deb3b159f6b7563c2a2
marcus bueschleb <ligi@ligi.de>
2010-07-30 11:23:03 +0200

    doc & cleanup

--- src/org/ligi/gobandroid/logic/GoDefinitions.java
+++ src/org/ligi/gobandroid/logic/GoDefinitions.java
@@ -19,35 +19,44 @@
 
 package org.ligi.gobandroid.logic;
 
+/**
+ * some definitions for the game of Go
+ * 
+ * @author ligi
+ *
+ */
 public class GoDefinitions {
 
 	public final static byte PLAYER_BLACK=1;
 	public final static byte PLAYER_WHITE=2;
 
-
 	public final static byte STONE_NONE=0;
 	public final static byte STONE_BLACK=1;
 	public final static byte STONE_WHITE=2;
-
 	
-	public final static byte[][] hoshis19x19= { 
+	private final static byte[][] hoshis19x19= { 
 			 {15,3}, {3,15}, {15,15} , {3,3} , {9,9}
 			,{3,9} , {15,9}, {9,3}   , {9,15}
 		};
 	
-	public final static  byte[][] hoshis13x13= { 
+	private final static  byte[][] hoshis13x13= { 
 			 {9,3}, {3,9}, {9,9} , {3,3} , {6,6}
 			,{3,6} , {9,6}, {6,3}   , {6,9}
 		};
 	
-	public final static byte[][] hoshis9x9= { 
+	private final static byte[][] hoshis9x9= { 
 			 {6,2}, {2,6}, {6,6} , {2,2} , {4,4}
 			,{2,4} , {6,4}, {4,2}   , {4,6}
 		};
-	
 
-	public final static byte[][] getHandicapArray(int size) {
-    	switch(size) {
+	/**
+	 * 
+	 * @param board_size - return handicap array for which board_size 
+	 * 
+	 * @return handicap array
+	 */
+	public final static byte[][] getHandicapArray(int board_size) {
+    	switch(board_size) {
     		case 9:
     			return hoshis9x9;
     		case 13:
@@ -55,8 +64,6 @@ public class GoDefinitions {
     		case 19:
     			return hoshis19x19;
     	}
-    	return null;
+    	return null; // no handicap definition for this size
     }
-    
-	
 }	
