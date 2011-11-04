e54e01b62e99a65863077351ac732234ebea3f48
ligi <ligi@ligi.de>
2011-11-04 15:25:00 +0100

    different menus for different use-cases

--- src/org/ligi/gobandroid_hd/ui/GoProblemActivity.java
+++ src/org/ligi/gobandroid_hd/ui/GoProblemActivity.java
@@ -1,8 +1,15 @@
 package org.ligi.gobandroid_hd.ui;
 
+import org.ligi.gobandroid_hd.R;
 import org.ligi.gobandroid_hd.logic.GoGame;
+import org.ligi.gobandroid_hd.ui.alerts.GameInfoAlert;
+
+import android.content.Intent;
+import android.support.v4.view.Menu;
+import android.support.v4.view.MenuItem;
 
 public class GoProblemActivity extends GoActivity {
+	
 	public byte doMoveWithUIFeedback(byte x,byte y) {
 		byte res=super.doMoveWithUIFeedback(x,y);
 		if (res==GoGame.MOVE_VALID)
@@ -13,4 +20,30 @@ public class GoProblemActivity extends GoActivity {
 		game.notifyGameChange();
 		return res;
 	}
+
+
+    @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+    	this.getMenuInflater().inflate(R.menu.ingame_tsumego, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+
+	public boolean onOptionsItemSelected(MenuItem item) {                                                                                                 
+        
+        switch (item.getItemId()) {                                                                                                                   
+                                                                                                                                                      
+        case R.id.menu_game_info:                                                                                                                           
+            GameInfoAlert.show(this,game);                                                                                                        
+            break;                                                                                                                                
+
+		case R.id.menu_settings:
+            startActivity(new Intent(this,GoPrefsActivity.class));
+            break;
+		}
+		
+		return false;
+	}
+    
+
 }
