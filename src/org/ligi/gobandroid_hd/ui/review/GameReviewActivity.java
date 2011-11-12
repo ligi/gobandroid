ab8ec09bb81a7b0c05ddb53bb03c1384ab843eb3
ligi <ligi@ligi.de>
2011-11-12 18:09:09 +0100

    do not allow the user to edit moves in review mode

--- src/org/ligi/gobandroid_hd/ui/review/GameReviewActivity.java
+++ src/org/ligi/gobandroid_hd/ui/review/GameReviewActivity.java
@@ -1,5 +1,6 @@
 package org.ligi.gobandroid_hd.ui.review;
 
+import org.ligi.gobandroid_hd.logic.GoGame;
 import org.ligi.gobandroid_hd.ui.GoActivity;
 import org.ligi.gobandroid_hd.ui.NavigationAndCommentFragment;
 import android.support.v4.app.Fragment;
@@ -10,4 +11,10 @@ public class GameReviewActivity extends GoActivity  {
 		return new NavigationAndCommentFragment();
 	}
 	
+	
+	@Override
+	public byte doMoveWithUIFeedback(byte x,byte y) {
+		// we want the user not to be able to edit in review mode
+		return GoGame.MOVE_VALID;
+	}	 
 }
