f53a3b1dfb1b8d257a18340c78c0932931aeb353
ligi <ligi@ligi.de>
2011-11-24 13:29:51 +0100

    rm unused import

--- src/org/ligi/gobandroid_hd/ui/GoActivity.java
+++ src/org/ligi/gobandroid_hd/ui/GoActivity.java
@@ -36,7 +36,6 @@ import org.ligi.gobandroid_hd.ui.recording.SaveSGFDialog;
 import org.ligi.tracedroid.logging.Log;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
