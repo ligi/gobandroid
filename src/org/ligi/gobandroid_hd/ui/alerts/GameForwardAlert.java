24c12490e8e5c1cb92c9b0b16ed3f04f6a6ead6b
ligi <ligi@ligi.de>
2011-11-26 12:51:31 +0100

    add files

---
+++ src/org/ligi/gobandroid_hd/ui/alerts/GameForwardAlert.java
@@ -0,0 +1,101 @@
+/**
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as 
+ * published by the Free Software Foundation; 
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ **/
+
+package org.ligi.gobandroid_hd.ui.alerts;
+
+import org.ligi.gobandroid_hd.R;
+import org.ligi.gobandroid_hd.logic.GoGame;
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+/**
+ * Dialog to show when user wants to go to next move - handles selection of move-variations
+ * 
+ * @author <a href="http://ligi.de">Marcus -Ligi- Bueschleb</a>
+ * 
+ * License: This software is licensed with GPLv3
+ * 
+ **/
+public class GameForwardAlert {
+
+	
+	public static void show(Context ctx,final GoGame game) {
+		if (!game.canRedo())
+			return ;
+		
+		if (game.getPossibleVariationCount()>0)	{
+		LinearLayout lin=new LinearLayout(ctx);
+		LinearLayout li=new LinearLayout(ctx);
+
+		TextView txt =new TextView(ctx);
+
+		// show the comment when there is one - useful for SGF game problems
+		if (game.getActMove().hasComment())
+			txt.setText(game.getActMove().getComment());
+		else
+			txt.setText("" +( game.getPossibleVariationCount()+1) + " Variations found for this move - which should we take?");
+	
+		txt.setPadding(10, 2, 10, 23);
+		lin.addView(txt);
+		lin.addView(li);
+		lin.setOrientation(LinearLayout.VERTICAL);
+		
+		final Dialog select_dlg=new Dialog(ctx);
+		final Boolean redoing=false;
+		View.OnClickListener var_select_listener=new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (redoing)
+					return;
+				select_dlg.hide();
+				if (!v.isEnabled()) return;
+				v.setEnabled(false	);
+				
+				game.redo((Integer)(v.getTag()));
+			}
+		};
+		
+		li.setWeightSum(1.0f*(game.getPossibleVariationCount()+1));
+		li.setLayoutParams(new LinearLayout.LayoutParams( LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+		
+		for (Integer i=0;i<game.getPossibleVariationCount()+1;i++)
+			{
+			Button var_btn=new Button(ctx);
+			var_btn.setTag(i);
+			var_btn.setOnClickListener(var_select_listener );
+			if (game.getActMove().getnextMove(i).isMarked())
+				var_btn.setText(game.getActMove().getnextMove(i).getMarkText());
+			else
+				var_btn.setText(""+(i+1));
+				
+			li.addView(var_btn);
+	
+			var_btn.setLayoutParams(new LinearLayout.LayoutParams( LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT,1f));
+			}
+
+		select_dlg.setTitle(R.string.variations);
+		select_dlg.setContentView(lin);
+		
+		select_dlg.show();
+	}
+	else
+		game.redo(0);
+	}
+}
---
+++ src/org/ligi/gobandroid_hd/ui/review/BookmarkDialog.java
@@ -0,0 +1,87 @@
+/**
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as 
+ * published by the Free Software Foundation; 
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ **/
+
+package org.ligi.gobandroid_hd.ui.review;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.ligi.gobandroid_hd.R;
+import org.ligi.gobandroid_hd.logic.GoGameProvider;
+import org.ligi.gobandroid_hd.ui.application.GobandroidFragmentActivity;
+import org.ligi.tracedroid.logging.Log;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.widget.EditText;
+/**
+ * Dialog to show when user wants to set a BookMark
+ * 
+ * @author <a href="http://ligi.de">Marcus -Ligi- Bueschleb</a>
+ * 
+ * License: This software is licensed with GPLv3
+ * 
+ **/
+public class BookmarkDialog {
+
+	public static void show(final GobandroidFragmentActivity ctx) {
+		final EditText fname_edit=new EditText(ctx);
+		
+		String fname=GoGameProvider.getGame().getMetaData().getFileName();
+		if ((fname==null)||	(fname.equals("")))
+			return; // need that to make sense
+		
+		String[] path_components=fname.split("/");
+		String inner_fname=path_components[path_components.length-1].replace(".sgf", "");
+	    fname_edit.setText(inner_fname);
+		
+		new AlertDialog.Builder(ctx).setTitle(R.string.menu_bookmark)
+		.setMessage("bookmark to write into " + ctx.getSettings().getBookmarkPath())
+		.setView(fname_edit)
+		.setPositiveButton(android.R.string.ok, new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				int move_pos=GoGameProvider.getGame().getActMove().getMovePos();
+			
+				File f = new File(ctx.getSettings().getBookmarkPath());
+				
+				if (!f.isDirectory())
+					f.mkdirs();
+				
+				try {
+					f=new File(ctx.getSettings().getBookmarkPath() + "/"+fname_edit.getText().toString()+".golink");
+					f.createNewFile();
+					
+					FileWriter sgf_writer = new FileWriter(f);
+					
+					BufferedWriter out = new BufferedWriter(sgf_writer);
+					
+					out.write(GoGameProvider.getGame().getMetaData().getFileName()+":#"+move_pos);
+					out.close();
+					sgf_writer.close();
+				}
+				catch (IOException e) {
+					Log.i(""+e);
+				}
+			}
+			
+		})
+		.show();
+	}
+}
