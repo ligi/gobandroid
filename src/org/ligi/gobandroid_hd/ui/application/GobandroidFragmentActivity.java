6f9c788a83737b0ed8ddfeb58ebf9326ac6bc490
ligi <ligi@ligi.de>
2011-11-30 23:46:28 +0100

    add home button action

--- src/org/ligi/gobandroid_hd/ui/application/GobandroidFragmentActivity.java
+++ src/org/ligi/gobandroid_hd/ui/application/GobandroidFragmentActivity.java
@@ -1,13 +1,16 @@
 package org.ligi.gobandroid_hd.ui.application;
 
+import org.ligi.gobandroid_hd.ui.gobandroid;
 import org.ligi.tracedroid.TraceDroid;
 import org.ligi.tracedroid.logging.Log;
 import org.ligi.tracedroid.sending.TraceDroidEmailSender;
 
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.MenuItem;
 
 public class GobandroidFragmentActivity extends FragmentActivity {
 
@@ -41,5 +44,18 @@ public class GobandroidFragmentActivity extends FragmentActivity {
 
 	public GobandroidSettings getSettings() {
 		return new GobandroidSettings(this);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	        case android.R.id.home:
+	            // app icon in action bar clicked; go home
+	            Intent intent = new Intent(this, gobandroid.class);
+	            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	            startActivity(intent);
+	            return true;
+	    }
+	    return super.onContextItemSelected(item);
 	}
 }
