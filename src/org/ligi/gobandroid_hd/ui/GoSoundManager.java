5ddef1b4872b651019719fb4d458c29f5d82018d
ligi <ligi@ligi.de>
2011-12-20 22:18:26 +0100

    add the sound manager

---
+++ src/org/ligi/gobandroid_hd/ui/GoSoundManager.java
@@ -0,0 +1,50 @@
+package org.ligi.gobandroid_hd.ui;
+
+import java.util.HashMap;
+
+import org.ligi.gobandroid_hd.R;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.SoundPool;
+
+public class GoSoundManager {
+	 
+	private  SoundPool mSoundPool;
+	private  HashMap<Integer, Integer> mSoundPoolMap;
+	private  AudioManager  mAudioManager;
+	private  Context mContext;
+	
+	public final static int SOUND_START=1;
+	public final static int SOUND_END=2;
+	public final static int SOUND_PLACE1=3;
+	public final static int SOUND_PLACE2=4;
+	public final static int SOUND_PICKUP1=5;
+	public final static int SOUND_PICKUP2=6;
+	
+	public GoSoundManager (Context theContext) {
+	    mContext = theContext;
+	    mSoundPool = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
+	    mSoundPoolMap = new HashMap<Integer, Integer> ();
+	    mAudioManager = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
+	    
+	    addSound(SOUND_START,R.raw.go_start);
+	    addSound(SOUND_END,R.raw.go_clearboard);
+	    addSound(SOUND_PLACE1,R.raw.go_place1);
+	    addSound(SOUND_PLACE2,R.raw.go_place2);
+	    addSound(SOUND_PICKUP1,R.raw.go_pickup1);
+	    addSound(SOUND_PICKUP2,R.raw.go_pickup2);
+	    
+	}
+	
+	public void addSound(int index, int SoundID) {
+	    mSoundPoolMap.put(index, mSoundPool.load(mContext, SoundID, 1));
+	}
+	
+	public void playSound(int index)
+	{
+	float streamVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+	streamVolume = streamVolume / mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+	    mSoundPool.play(mSoundPoolMap.get(index), streamVolume, streamVolume, 1, 0, 1f);
+	}
+}
