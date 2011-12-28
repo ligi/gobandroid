c92a9ac17bd3bbfcb2aa1848817e68670389c457
ligi <ligi@ligi.de>
2011-12-28 02:49:53 +0100

    use fkt from Lib

--- src/org/ligi/gobandroid_hd/ui/GoSetupActivity.java
+++ src/org/ligi/gobandroid_hd/ui/GoSetupActivity.java
@@ -20,6 +20,8 @@
 package org.ligi.gobandroid_hd.ui;
 
 import java.util.List;
+
+import org.ligi.android.common.intents.IntentHelper;
 import org.ligi.gobandroid_hd.R;
 import org.ligi.gobandroid_hd.logic.GnuGoMover;
 import org.ligi.gobandroid_hd.logic.GoGame;
@@ -86,27 +88,6 @@ public class GoSetupActivity extends GobandroidFragmentActivity implements OnSee
 	
 	private TextView handicap_text;
 	
-	/**
-	 * Indicates whether the specified action can be used as an intent. This
-	 * method queries the package manager for installed packages that can
-	 * respond to an intent with the specified action. If no suitable package is
-	 * found, this method returns false.
-	 *
-	 * @param context The application's environment.
-	 * @param action The Intent action to check for availability.
-	 *
-	 * @return True if an Intent with the specified action can be sent and
-	 *         responded to, false otherwise.
-	 */
-	public static boolean isIntentAvailable(Context context, String action) {
-	    final PackageManager packageManager = context.getPackageManager();
-	    final Intent intent = new Intent(action);
-	    List<ResolveInfo> list =
-	            packageManager.queryIntentServices(intent,PackageManager.GET_SERVICES);
-	                    
-	    return list.size() > 0;
-	}
-	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -164,7 +145,7 @@ public class GoSetupActivity extends GobandroidFragmentActivity implements OnSee
 		black_player_spinner=(Spinner)this.findViewById(R.id.BlackPlayerSpinner);
 	
 		String[] player_strings;
-		if (isIntentAvailable(this,GnuGoMover.intent_action_name))
+		if (IntentHelper.isIntentAvailable(this.getPackageManager() ,new Intent(GnuGoMover.intent_action_name)))
 			player_strings=new String[] {"Phone","GnuGo"};
 		else
 			player_strings=new String[] {"Phone"};
