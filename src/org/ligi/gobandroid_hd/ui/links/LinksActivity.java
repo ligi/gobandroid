efb46cb12d3bf20db12a96ab1500c4dcb77543f6
ligi <ligi@ligi.de>
2012-04-05 16:15:08 +0200

    add japanese translation and credits

--- src/org/ligi/gobandroid_hd/ui/links/LinksActivity.java
+++ src/org/ligi/gobandroid_hd/ui/links/LinksActivity.java
@@ -75,6 +75,7 @@ public class LinksActivity extends GobandroidFragmentActivity implements ActionB
 					new LinkWithDescription("https://github.com/ligi/gobandroid","Code/Issues on GitHub"),
 					new LinkWithDescriptionAndTitle("http://gogameguru.com/","source of default Tsumego and commented game SGF's","gogameguru.com"),
 					new LinkWithDescriptionAndTitle("http://actionbarsherlock.com/","library used for ActionBar backward compatibility ","ActionBarSherlock"),
+					new LinkWithDescriptionAndTitle("http://plus.google.com/107473613683165260026","Japanese Translation","Hiroki Ino"),
 					new LinkWithDescriptionAndTitle("https://plus.google.com/102354246669329539149","Chinese Translation","Noorudin Ma"),
 					new LinkWithDescriptionAndTitle("http://transimple.de","German Translation","Dirk Blasejezak"),		
 					new LinkWithDescriptionAndTitle("https://plus.google.com/109272815840179446675","French Translation #1","Sylvain Soliman"),
