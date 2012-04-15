2fe9f89856d4b4fa330aef21cd34d0537ad31d23
ligi <ligi@ligi.de>
2012-04-15 13:59:00 +0200

    move gokifu away from top - so that on top is a site with easy clickable SGF's

--- src/org/ligi/gobandroid_hd/ui/links/LinksActivity.java
+++ src/org/ligi/gobandroid_hd/ui/links/LinksActivity.java
@@ -95,14 +95,16 @@ public class LinksActivity extends GobandroidFragmentActivity implements ActionB
 				links=new LinkWithDescription[] {
 
 					// source pro games
-					new LinkWithDescription("http://gokifu.com/","gokifu"),
 					new LinkWithDescription("http://www.andromeda.com/people/ddyer/age-summer-94/companion.html","Companion"),
+					
 					new LinkWithDescription("http://homepages.cwi.nl/~aeb/go/games/games/Judan/","Judan"),
 					new LinkWithDescription("http://gogameworld.com/gophp/pg_samplegames.php","Commented gogameworld sample games"),
 					new LinkWithDescription("http://sites.google.com/site/byheartgo/","byheartgo"),
-								
+					new LinkWithDescription("http://gokifu.com/","gokifu"),
+					
 					// problems
 					new LinkWithDescription("http://www.usgo.org/problems/index.html","USGo Problems" ),
+					
 					
 					//mixed
 					new LinkWithDescription("http://www.britgo.org/bgj/recent.html","Britgo recent")
