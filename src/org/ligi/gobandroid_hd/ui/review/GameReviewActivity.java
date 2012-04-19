3d88597011d680f47e66ac976466110a0e6339b7
ligi <ligi@ligi.de>
2012-04-19 19:02:44 +0200

    rm unused imports

--- src/org/ligi/gobandroid_hd/ui/review/GameReviewActivity.java
+++ src/org/ligi/gobandroid_hd/ui/review/GameReviewActivity.java
@@ -2,13 +2,10 @@ package org.ligi.gobandroid_hd.ui.review;
 
 import org.ligi.gobandroid_hd.R;
 import org.ligi.gobandroid_hd.logic.GoGame;
-import org.ligi.gobandroid_hd.logic.GoGameProvider;
 import org.ligi.gobandroid_hd.ui.GoActivity;
 import org.ligi.gobandroid_hd.ui.NavigationAndCommentFragment;
 import org.ligi.gobandroid_hd.ui.ZoomGameExtrasFragment;
 import org.ligi.gobandroid_hd.ui.alerts.GameForwardAlert;
-import org.ligi.tracedroid.logging.Log;
-
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 
