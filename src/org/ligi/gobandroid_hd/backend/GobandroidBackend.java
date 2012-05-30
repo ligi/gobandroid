59337073dbcd8b34b25c9f6f06fc52f99c66e5fb
ligi <ligi@ligi.de>
2012-05-30 21:07:33 +0200

    do the C2DM registering in AsyncTask to prevent ANR

--- src/org/ligi/gobandroid_hd/backend/GobandroidBackend.java
+++ src/org/ligi/gobandroid_hd/backend/GobandroidBackend.java
@@ -11,6 +11,7 @@ import org.ligi.tracedroid.Log;
 
 import com.google.android.c2dm.C2DMessaging;
 
+import android.os.AsyncTask;
 import android.os.Build;
 import android.provider.Settings.Secure;
 
@@ -43,34 +44,48 @@ public class GobandroidBackend {
 		}
 	}
 	
-	public final static boolean registerDevice(GobandroidApp app) {
-		try {
-			
-			String device_id=Secure.getString( app.getContentResolver(), Secure.ANDROID_ID);
-			String push_id=C2DMessaging.getRegistrationId(app);
-			if (push_id.equals(""))
-				push_id="unknown";
-			
-			String url_str="https://"+GobandroidConfiguration.backend_domain+"/push/register?";
-			url_str+=getURLParamSnippet("device_id",device_id);
-			url_str+="&"+getURLParamSnippet("push_key",push_id);
-			url_str+="&"+getURLParamSnippet("app_version",app.getAppVersion());
-			
-			String wanted_pushs="";
-			if (app.getSettings().isTsumegoPushEnabled())
-				wanted_pushs+="tsumego";
-			if (wanted_pushs.equals(""))
-				wanted_pushs="none";
-			
-			url_str+="&"+getURLParamSnippet("wanted_pushs",wanted_pushs);
-			url_str+="&"+getURLParamSnippet("device_str",Build.VERSION.RELEASE + " | " + Build.MANUFACTURER + " | " + Build.DEVICE  + " | " + Build.MODEL + " | " + Build.DISPLAY + " | " + Build.CPU_ABI +" | "+Build.TYPE + " | " +Build.TAGS);
-			
-			
-			URL url=new URL(url_str);
-			return NetHelper.downloadURL2String(url).replace("\n","").replace("\r","").equals("saved");
-		} catch (Exception e) {
-			Log.w("cannot register push" +e );
-			return false;
+	static class RegisterDeviceAsyncTask extends AsyncTask<Void,Void,Void> {
+
+		private GobandroidApp app;
+		
+		public RegisterDeviceAsyncTask(GobandroidApp app) {
+			this.app=app;
 		}
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				
+				String device_id=Secure.getString( app.getContentResolver(), Secure.ANDROID_ID);
+				String push_id=C2DMessaging.getRegistrationId(app);
+				if (push_id.equals(""))
+					push_id="unknown";
+				
+				String url_str="https://"+GobandroidConfiguration.backend_domain+"/push/register?";
+				url_str+=getURLParamSnippet("device_id",device_id);
+				url_str+="&"+getURLParamSnippet("push_key",push_id);
+				url_str+="&"+getURLParamSnippet("app_version",app.getAppVersion());
+				
+				String wanted_pushs="";
+				if (app.getSettings().isTsumegoPushEnabled())
+					wanted_pushs+="tsumego";
+				if (wanted_pushs.equals(""))
+					wanted_pushs="none";
+				
+				url_str+="&"+getURLParamSnippet("wanted_pushs",wanted_pushs);
+				url_str+="&"+getURLParamSnippet("device_str",Build.VERSION.RELEASE + " | " + Build.MANUFACTURER + " | " + Build.DEVICE  + " | " + Build.MODEL + " | " + Build.DISPLAY + " | " + Build.CPU_ABI +" | "+Build.TYPE + " | " +Build.TAGS);
+				
+				
+				URL url=new URL(url_str);
+				NetHelper.downloadURL2String(url).replace("\n","").replace("\r","");//.equals("saved");
+			} catch (Exception e) {
+				Log.w("cannot register push" +e );
+			}
+			return null;
+		}
+		
+	}
+	public final static void registerDevice(GobandroidApp app) {
+		new RegisterDeviceAsyncTask(app).execute();
 	}
 }
