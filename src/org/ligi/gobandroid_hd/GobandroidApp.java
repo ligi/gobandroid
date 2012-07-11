4fd8c9091552fa2627dbedb5fe1a5cfc043291b6
ligi <ligi@ligi.de>
2012-07-11 03:37:11 +0200

    improve GCM init ( handle SDK <87 & other exceptions )

--- src/org/ligi/gobandroid_hd/GobandroidApp.java
+++ src/org/ligi/gobandroid_hd/GobandroidApp.java
@@ -8,11 +8,12 @@ import org.ligi.tracedroid.logging.Log;
 
 import android.app.Application;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Build;
 
+import com.google.analytics.tracking.android.EasyTracker;
 import com.google.android.gcm.GCMRegistrar;
 
 public class GobandroidApp extends Application {
-
 	
 	private InteractionScope interaction_scope;   // holds things like mode/act game between activitysw
 	
@@ -32,26 +33,34 @@ public class GobandroidApp extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		EasyTracker.getInstance().setContext(this); 
+		
 		TraceDroid.init(this);
 	    Log.setTAG("gobandroid");
         
         interaction_scope=new InteractionScope();
 
-        // Make sure the device has the proper dependencies.                                                                
-        GCMRegistrar.checkDevice(this);
-        // Make sure the manifest was properly set - comment out this line                                                  
-        // while developing the app, then uncomment it when it's ready.                                                     
-        GCMRegistrar.checkManifest(this);
-        //setContentView(R.layout.main);
         
-        final String regId = GCMRegistrar.getRegistrationId(this);
-        if (regId.equals("")) {
-            // Automatically registers application on startup.                                                              
-            GCMRegistrar.register(this, SENDER_ID);
-        }
-        Log.i("gobandroid GCM regid  " + regId);
+        if (Build.VERSION.SDK_INT>7)	// need at least 8 for C2DM
+        	initGCM();
         
-	    GobandroidBackend.registerDevice(this);
+        GobandroidBackend.registerDevice(this);
+	}
+	
+	
+	private void initGCM() {
+		try {
+		     // Make sure the device has the proper dependencies.                                                                
+	        GCMRegistrar.checkDevice(this);
+	        
+	        final String regId = GCMRegistrar.getRegistrationId(this);
+	        if (regId.equals("")) {
+	            // Automatically registers application on startup.                                                              
+	            GCMRegistrar.register(this, SENDER_ID);
+	        }
+		} catch ( Exception e) {
+			EasyTracker.getTracker().trackException("cannot init GCM", e,false);
+		}
 	}
 
 	@Override
