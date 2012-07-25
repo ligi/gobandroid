586d50dbb6f841f90cf9bc8c67d2e0231086909a
ligi <ligi@ligi.de>
2012-07-25 19:15:22 +0200

    always show the overflow icon  because I strongly disagree with the style guide here

--- src/org/ligi/gobandroid_hd/ui/application/GobandroidFragmentActivity.java
+++ src/org/ligi/gobandroid_hd/ui/application/GobandroidFragmentActivity.java
@@ -1,5 +1,7 @@
 package org.ligi.gobandroid_hd.ui.application;
 
+import java.lang.reflect.Field;
+
 import org.ligi.gobandroid_hd.GobandroidApp;
 import org.ligi.gobandroid_hd.logic.GoGame;
 import org.ligi.gobandroid_hd.ui.gobandroid;
@@ -7,6 +9,7 @@ import org.ligi.gobandroid_hd.ui.gobandroid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.ViewConfiguration;
 import android.view.WindowManager;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
@@ -19,6 +22,20 @@ public class GobandroidFragmentActivity extends SherlockFragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // a little hack because I strongly disagree with the style guide here ;-)
+        // not having the Actionbar overfow menu also with devices with hardware key really helps discoverability
+        // http://stackoverflow.com/questions/9286822/how-to-force-use-of-overflow-menu-on-devices-with-menu-button
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            if(menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(config, false);
+            }
+        } catch (Exception ex) {
+            // Ignore - but at least we tried ;-)
+        }
+        
         // we do not want focus on custom views ( mainly for GTV )
         if ((this.getSupportActionBar()!=null) && (this.getSupportActionBar().getCustomView()!=null))
         	this.getSupportActionBar().getCustomView().setFocusable(false);
