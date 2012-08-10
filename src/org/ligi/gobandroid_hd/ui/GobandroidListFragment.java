e054769091ad578a59e9e3e0b322fb8b5986b755
ligi <ligi@ligi.de>
2012-08-10 22:49:23 +0200

    improve the list-divider

---
+++ src/org/ligi/gobandroid_hd/ui/GobandroidListFragment.java
@@ -0,0 +1,19 @@
+package org.ligi.gobandroid_hd.ui;
+
+import org.ligi.gobandroid_hd.R;
+
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+
+public class GobandroidListFragment extends ListFragment {
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		getListView().setCacheColorHint(0);
+        getListView().setDivider((GradientDrawable)getActivity().getResources().getDrawable(R.drawable.divider));
+        getListView().setDividerHeight(2);
+        
+	}
+}
--- src/org/ligi/gobandroid_hd/ui/sgf_listing/SGFListFragment.java
+++ src/org/ligi/gobandroid_hd/ui/sgf_listing/SGFListFragment.java
@@ -1,11 +1,15 @@
 package org.ligi.gobandroid_hd.ui.sgf_listing;
 
 import java.io.File;
+
+import org.ligi.gobandroid_hd.R;
 import org.ligi.gobandroid_hd.GobandroidApp;
 import org.ligi.gobandroid_hd.InteractionScope;
 import org.ligi.gobandroid_hd.ui.GoLinkLoadActivity;
+import org.ligi.gobandroid_hd.ui.GobandroidListFragment;
 import org.ligi.gobandroid_hd.ui.SGFLoadActivity;
 import android.content.Intent;
+import android.graphics.drawable.ShapeDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
@@ -13,7 +17,7 @@ import android.view.View;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
-public class SGFListFragment extends ListFragment{
+public class SGFListFragment extends GobandroidListFragment{
 
 	private String[] menu_items;
     private String dir;
@@ -29,7 +33,7 @@ public class SGFListFragment extends ListFragment{
 	
 		    	
 	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		InteractionScope interaction_scope=((GobandroidApp)(getActivity().getApplicationContext())).getInteractionScope();
@@ -46,8 +50,7 @@ public class SGFListFragment extends ListFragment{
 			 adapter=new ReviewPathViewAdapter(this.getActivity(), menu_items,dir);
 		 
         this.setListAdapter(adapter);
-        
-        this.getListView().setCacheColorHint(0);
+     
 	}
 	
 	@Override
