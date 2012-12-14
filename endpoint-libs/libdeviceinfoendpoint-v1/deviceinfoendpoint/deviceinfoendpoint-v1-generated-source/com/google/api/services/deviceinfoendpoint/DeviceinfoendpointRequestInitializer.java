6f666ef777c238e0821c12488d76e5f9a0ac0bd1
ligi <ligi@ligi.de>
2012-12-14 19:55:59 +0100

    change color

---
+++ endpoint-libs/libdeviceinfoendpoint-v1/deviceinfoendpoint/deviceinfoendpoint-v1-generated-source/com/google/api/services/deviceinfoendpoint/DeviceinfoendpointRequestInitializer.java
@@ -0,0 +1,123 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+/*
+ * This file was generated.
+ *  with google-apis-code-generator 1.2.0 (build: 2012-11-27 16:02:30 UTC)
+ *  on 2012-12-09 at 20:31:02 UTC 
+ */
+
+package com.google.api.services.deviceinfoendpoint;
+
+import com.google.api.client.googleapis.services.json.AbstractGoogleJsonClientRequest;
+import com.google.api.client.googleapis.services.json.CommonGoogleJsonClientRequestInitializer;
+
+/**
+ * Deviceinfoendpoint request initializer for setting properties like key and userIp.
+ *
+ * <p>
+ * The simplest usage is to use it to set the key parameter:
+ * </p>
+ *
+ * <pre>
+  public static final GoogleClientRequestInitializer KEY_INITIALIZER =
+      new DeviceinfoendpointRequestInitializer(KEY);
+ * </pre>
+ *
+ * <p>
+ * There is also a constructor to set both the key and userIp parameters:
+ * </p>
+ *
+ * <pre>
+  public static final GoogleClientRequestInitializer INITIALIZER =
+      new DeviceinfoendpointRequestInitializer(KEY, USER_IP);
+ * </pre>
+ *
+ * <p>
+ * If you want to implement custom logic, extend it like this:
+ * </p>
+ *
+ * <pre>
+  public static class MyRequestInitializer extends DeviceinfoendpointRequestInitializer {
+
+    {@literal @}Override
+    public void initializeDeviceinfoendpointRequest(DeviceinfoendpointRequest{@literal <}?{@literal >} request)
+        throws IOException {
+      // custom logic
+    }
+  }
+ * </pre>
+ *
+ * <p>
+ * Finally, to set the key and userIp parameters and insert custom logic, extend it like this:
+ * </p>
+ *
+ * <pre>
+  public static class MyRequestInitializer2 extends DeviceinfoendpointRequestInitializer {
+
+    public MyKeyRequestInitializer() {
+      super(KEY, USER_IP);
+    }
+
+    {@literal @}Override
+    public void initializeDeviceinfoendpointRequest(DeviceinfoendpointRequest{@literal <}?{@literal >} request)
+        throws IOException {
+      // custom logic
+    }
+  }
+ * </pre>
+ *
+ * <p>
+ * Subclasses should be thread-safe.
+ * </p>
+ *
+ * @since 1.12
+ */
+public class DeviceinfoendpointRequestInitializer extends CommonGoogleJsonClientRequestInitializer {
+
+  public DeviceinfoendpointRequestInitializer() {
+    super();
+  }
+
+  /**
+   * @param key API key or {@code null} to leave it unchanged
+   */
+  public DeviceinfoendpointRequestInitializer(String key) {
+    super(key);
+  }
+
+  /**
+   * @param key API key or {@code null} to leave it unchanged
+   * @param userIp user IP or {@code null} to leave it unchanged
+   */
+  public DeviceinfoendpointRequestInitializer(String key, String userIp) {
+    super(key, userIp);
+  }
+
+  @Override
+  public final void initializeJsonRequest(AbstractGoogleJsonClientRequest<?> request) throws java.io.IOException {
+    super.initializeJsonRequest(request);
+    initializeDeviceinfoendpointRequest((DeviceinfoendpointRequest<?>) request);
+  }
+
+  /**
+   * Initializes Deviceinfoendpoint request.
+   *
+   * <p>
+   * Default implementation does nothing. Called from
+   * {@link #initializeJsonRequest(AbstractGoogleJsonClientRequest)}.
+   * </p>
+   *
+   * @throws java.io.IOException I/O exception
+   */
+  protected void initializeDeviceinfoendpointRequest(DeviceinfoendpointRequest<?> request) throws java.io.IOException {
+  }
+}
