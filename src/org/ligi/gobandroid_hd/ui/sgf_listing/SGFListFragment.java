f370fb1415b717d78917a93505fa5808ecb1f056
ligi <ligi@ligi.de>
2013-01-28 20:38:04 +0100

    move the refresh to the fragment so we can cleanly refresh after delete

--- src/org/ligi/gobandroid_hd/ui/sgf_listing/SGFListFragment.java
+++ src/org/ligi/gobandroid_hd/ui/sgf_listing/SGFListFragment.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import org.ligi.android.common.activitys.ActivityFinishOnCancelListener;
+import org.ligi.android.common.arrays.ArrayHelper;
+import org.ligi.android.common.dialogs.ActivityFinishOnDialogClickListener;
 import org.ligi.android.common.dialogs.DialogDiscarder;
 import org.ligi.gobandroid_hd.GobandroidApp;
 import org.ligi.gobandroid_hd.InteractionScope;
@@ -17,20 +20,24 @@ import org.ligi.gobandroid_hd.ui.GoLinkLoadActivity;
 import org.ligi.gobandroid_hd.ui.GobandroidListFragment;
 import org.ligi.gobandroid_hd.ui.SGFLoadActivity;
 import org.ligi.gobandroid_hd.ui.alerts.ShareSGFDialog;
+import org.ligi.gobandroid_hd.ui.review.SGFMetaData;
+import org.ligi.tracedroid.Log;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Vector;
 
 public class SGFListFragment extends GobandroidListFragment {
 
     private String[] menu_items;
     private String dir;
     private BaseAdapter adapter;
+    private File[] files;
 
     public SGFListFragment() {
     }
 
-    public SGFListFragment(String[] menu_items, File dir) {
-        this.menu_items = menu_items;
+    public SGFListFragment(File dir) {
         this.dir = dir.getAbsolutePath();
     }
 
@@ -40,11 +47,11 @@ public class SGFListFragment extends GobandroidListFragment {
 
         InteractionScope interaction_scope = ((GobandroidApp) (getActivity().getApplicationContext())).getInteractionScope();
 
-        if (menu_items == null)
-            menu_items = savedInstanceState.getStringArray("menu_items");
+        if (savedInstanceState != null)
+            getEnvFromSavedInstance(savedInstanceState);
 
-        if (dir == null)
-            dir = savedInstanceState.getString("dir");
+        if (menu_items == null) // we got nothing from savedInstance
+            refresh();
 
         if (interaction_scope.getMode() == InteractionScope.MODE_TSUMEGO)
             adapter = new TsumegoPathViewAdapter(this.getActivity(), menu_items, dir);
@@ -53,6 +60,14 @@ public class SGFListFragment extends GobandroidListFragment {
 
         this.setListAdapter(adapter);
 
+    }
+
+    private void getEnvFromSavedInstance(Bundle savedInstanceState) {
+        if (menu_items == null)
+            menu_items = savedInstanceState.getStringArray("menu_items");
+
+        if (dir == null)
+            dir = savedInstanceState.getString("dir");
     }
 
     @Override
@@ -76,6 +91,7 @@ public class SGFListFragment extends GobandroidListFragment {
                                             @Override
                                             public void onClick(DialogInterface dialog, int which) {
                                                 new File(dir + "/" + menu_items[position]).delete();
+                                                refresh();
                                             }
                                         })
 
@@ -117,4 +133,70 @@ public class SGFListFragment extends GobandroidListFragment {
         outState.putStringArray("menu_items", menu_items);
         outState.putString("dir", dir);
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        refresh();
+    }
+
+
+    public void refresh() {
+        Log.i("refresh list");
+        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity()).setTitle(R.string.problem_listing_sgf);
+
+        alert.setPositiveButton(R.string.ok, new ActivityFinishOnDialogClickListener(getActivity()));
+        alert.setOnCancelListener(new ActivityFinishOnCancelListener(getActivity()));
+
+        if (dir == null) {
+            alert.setMessage(getResources().getString(R.string.sgf_path_invalid) + " " + dir).show();
+            return;
+        }
+
+        File dir_file = new File(dir);
+
+        files = new File(dir).listFiles();
+
+        if (files == null) {
+            alert.setMessage(getResources().getString(R.string.there_are_no_files_in) + " " + dir_file.getAbsolutePath()).show();
+            return;
+        }
+
+        Vector<String> fnames = new Vector<String>();
+        for (File file : files)
+            if ((file.getName().endsWith(".sgf")) || (file.isDirectory()) || (file.getName().endsWith(".golink"))) {
+                fnames.add(file.getName());
+                Log.i("refresh adding + " + file.getName());
+            }
+
+        if (fnames.size() == 0) {
+            alert.setMessage(getResources().getString(R.string.there_are_no_files_in) + " " + dir_file.getAbsolutePath()).show();
+            return;
+        }
+
+
+        if (getApp().getInteractionScope().getMode() == InteractionScope.MODE_TSUMEGO) {
+            Vector<String> done = new Vector<String>(), undone = new Vector<String>();
+            for (String fname : fnames)
+                if (new SGFMetaData(dir_file.getAbsolutePath() + "/" + fname).is_solved)
+                    done.add(fname);
+                else
+                    undone.add(fname);
+
+            String[] undone_arr = (String[]) undone.toArray(new String[undone.size()]), done_arr = (String[]) done.toArray(new String[done.size()]);
+            Arrays.sort(undone_arr);
+            Arrays.sort(done_arr);
+            menu_items = ArrayHelper.concat(undone_arr, done_arr);
+        } else {
+            menu_items = (String[]) fnames.toArray(new String[fnames.size()]);
+            Arrays.sort(menu_items);
+        }
+
+
+    }
+
+    private GobandroidApp getApp() {
+        return (GobandroidApp) getActivity().getApplicationContext();
+    }
+
 }
--- src/org/ligi/gobandroid_hd/ui/sgf_listing/SGFSDCardListActivity.java
+++ src/org/ligi/gobandroid_hd/ui/sgf_listing/SGFSDCardListActivity.java
@@ -19,27 +19,19 @@
 
 package org.ligi.gobandroid_hd.ui.sgf_listing;
 
-import android.app.AlertDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
-import org.ligi.android.common.activitys.ActivityFinishOnCancelListener;
-import org.ligi.android.common.arrays.ArrayHelper;
-import org.ligi.android.common.dialogs.ActivityFinishOnDialogClickListener;
 import org.ligi.gobandroid_hd.InteractionScope;
 import org.ligi.gobandroid_hd.R;
 import org.ligi.gobandroid_hd.ui.GobandroidNotifications;
 import org.ligi.gobandroid_hd.ui.Refreshable;
 import org.ligi.gobandroid_hd.ui.application.GobandroidFragmentActivity;
-import org.ligi.gobandroid_hd.ui.review.SGFMetaData;
 import org.ligi.gobandroid_hd.ui.tsumego.fetch.DownloadProblemsDialog;
 import org.ligi.gobandroid_hd.ui.tsumego.fetch.TsumegoSource;
-import org.ligi.tracedroid.logging.Log;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.Vector;
 
 /**
  * Activity to load SGF's from SD card
@@ -47,10 +39,10 @@ import java.util.Vector;
  * @author <a href="http://ligi.de">Marcus -Ligi- Bueschleb</a>
  */
 
-public class SGFSDCardListActivity extends GobandroidFragmentActivity implements Refreshable {
+public class SGFSDCardListActivity extends GobandroidFragmentActivity {
 
     private String[] menu_items;
-    private File[] files;
+
     private File dir;
     private SGFListFragment list_fragment;
     private String sgf_path;
@@ -75,6 +67,30 @@ public class SGFSDCardListActivity extends GobandroidFragmentActivity implements
         if (sgf_path.substring(sgf_path.indexOf('/')).startsWith(getSettings().getReviewPath().substring(sgf_path.indexOf('/'))))
             this.getApp().getInteractionScope().setMode(InteractionScope.MODE_REVIEW);
         dir = new File(sgf_path);
+
+        setActionbarProperties();
+
+        list_fragment = new SGFListFragment(dir);
+        getSupportFragmentManager().beginTransaction().replace(R.id.list_fragment, list_fragment).commit();
+    }
+
+    private void setActionbarProperties() {
+        switch (getApp().getInteractionScope().getMode()) {
+            case InteractionScope.MODE_TSUMEGO:
+                this.setTitle(R.string.load_tsumego);
+                break;
+            default:
+                // we can only show stuff for tsumego and review - if in doubt -
+                // trade as review
+                getApp().getInteractionScope().setMode(InteractionScope.MODE_REVIEW);
+                // fall wanted
+
+            case InteractionScope.MODE_REVIEW:
+                this.setTitle(R.string.load_game);
+                break;
+        }
+
+        this.getSupportActionBar().setSubtitle(dir.getAbsolutePath());
     }
 
     @Override
@@ -105,81 +121,5 @@ public class SGFSDCardListActivity extends GobandroidFragmentActivity implements
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        refresh();
-    }
 
-    @Override
-    public void refresh() {
-
-        switch (getApp().getInteractionScope().getMode()) {
-            case InteractionScope.MODE_TSUMEGO:
-                this.setTitle(R.string.load_tsumego);
-                break;
-            default:
-                // we can only show stuff for tsumego and review - if in doubt -
-                // trade as review
-                getApp().getInteractionScope().setMode(InteractionScope.MODE_REVIEW);
-                // fall wanted
-
-            case InteractionScope.MODE_REVIEW:
-                this.setTitle(R.string.load_game);
-                break;
-        }
-
-        Log.i("refresh list");
-        AlertDialog.Builder alert = new AlertDialog.Builder(this).setTitle(R.string.problem_listing_sgf);
-
-        alert.setPositiveButton(R.string.ok, new ActivityFinishOnDialogClickListener(this));
-        alert.setOnCancelListener(new ActivityFinishOnCancelListener(this));
-
-        if (dir == null) {
-            alert.setMessage(getResources().getString(R.string.sgf_path_invalid) + " " + sgf_path).show();
-            return;
-        }
-
-        files = dir.listFiles();
-
-        if (files == null) {
-            alert.setMessage(getResources().getString(R.string.there_are_no_files_in) + " " + dir.getAbsolutePath()).show();
-            return;
-        }
-
-        Vector<String> fnames = new Vector<String>();
-        for (File file : files)
-            if ((file.getName().endsWith(".sgf")) || (file.isDirectory()) || (file.getName().endsWith(".golink"))) {
-                fnames.add(file.getName());
-                Log.i("refresh adding + " + file.getName());
-            }
-
-        if (fnames.size() == 0) {
-            alert.setMessage(getResources().getString(R.string.there_are_no_files_in) + " " + dir.getAbsolutePath()).show();
-            return;
-        }
-
-        this.getSupportActionBar().setSubtitle(dir.getAbsolutePath());
-
-        if (getApp().getInteractionScope().getMode() == InteractionScope.MODE_TSUMEGO) {
-            Vector<String> done = new Vector<String>(), undone = new Vector<String>();
-            for (String fname : fnames)
-                if (new SGFMetaData(dir.getAbsolutePath() + "/" + fname).is_solved)
-                    done.add(fname);
-                else
-                    undone.add(fname);
-
-            String[] undone_arr = (String[]) undone.toArray(new String[undone.size()]), done_arr = (String[]) done.toArray(new String[done.size()]);
-            Arrays.sort(undone_arr);
-            Arrays.sort(done_arr);
-            menu_items = ArrayHelper.concat(undone_arr, done_arr);
-        } else {
-            menu_items = (String[]) fnames.toArray(new String[fnames.size()]);
-            Arrays.sort(menu_items);
-        }
-
-        list_fragment = new SGFListFragment(menu_items, dir);
-        getSupportFragmentManager().beginTransaction().replace(R.id.list_fragment, list_fragment).commit();
-
-    }
 }
