df05eedc041e1a0e9ea796b04679b427591b0f89
ligi <ligi@ligi.de>
2014-07-07 23:46:06 +0200

    improve code

--- android/src/main/java/org/ligi/gobandroid_hd/ui/tsumego/NextTsumegoDeterminator.java
+++ android/src/main/java/org/ligi/gobandroid_hd/ui/tsumego/NextTsumegoDeterminator.java
@@ -1,11 +1,12 @@
 package org.ligi.gobandroid_hd.ui.tsumego;
 
+import com.google.common.base.Optional;
+
 import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NextTsumegoDeterminator {
-
 
     /**
      * try to find next tsumego based on filename
@@ -15,56 +16,52 @@ public class NextTsumegoDeterminator {
      * @return the filename found
      */
     public static String calcNextTsumego(String FileName) {
-        String old_index = getLastNumberInStringOrNull(FileName);
+        final Optional<String> old_index = getLastNumberInStringOrNull(FileName);
 
-        if (old_index == null) {
+        if (!old_index.isPresent()) {
             return null;
         }
 
-        int index = Integer.parseInt(old_index);
+        final int index = Integer.parseInt(old_index.get());
 
         String new_index = "";
         // add the leading zeroes
-        for (int i = 0; i < old_index.length() - ((index + 1) / 10 + 1); i++) {
+        for (int i = 0; i < old_index.get().length() - ((index + 1) / 10 + 1); i++) {
             new_index += "0";
         }
 
         new_index += "" + (index + 1);
 
-        String guessed_fname_str = replaceLast(FileName, old_index, new_index);
+        final String guessedFileNameString = replaceLast(FileName, old_index.get(), new_index);
 
         // check if it exists
-        if (!new File(guessed_fname_str).exists()) {
+        if (!new File(guessedFileNameString).exists()) {
             return null;
         }
 
-        return guessed_fname_str;
+        return guessedFileNameString;
 
     }
 
-    private static String getLastNumberInStringOrNull(String fname) {
-        Pattern p = Pattern.compile("\\d+");
-        Matcher m = p.matcher(fname);
+    private static Optional<String> getLastNumberInStringOrNull(final String fileName) {
+        final Pattern p = Pattern.compile("\\d+");
+        final Matcher m = p.matcher(fileName);
 
-        String old_index = "";
+        Optional<String> old_index = Optional.absent();
+
         while (m.find()) {
-            old_index = m.group();
-        }
-
-        // found no index -> exit
-        if (old_index.equals("")) {
-            return null;
+            old_index = Optional.of(m.group());
         }
 
         return old_index;
     }
 
-    private  static String replaceLast(String string, String from, String to) {
+    private static String replaceLast(String string, String from, String to) {
         int lastIndex = string.lastIndexOf(from);
         if (lastIndex < 0) {
             return string;
         }
-        String tail = string.substring(lastIndex).replaceFirst(from, to);
+        final String tail = string.substring(lastIndex).replaceFirst(from, to);
         return string.substring(0, lastIndex) + tail;
     }
 
