e4896a1150ae97fa9bb0166b1e58d62520321376
ligi <ligi@ligi.de>
2014-09-14 13:46:51 +0200

    add basic test for GoTerminology

---
+++ android/src/androidTest/java/org/ligi/gobandroidhd/uitest/TheGoTerminologyActivity.java
@@ -0,0 +1,28 @@
+package org.ligi.gobandroidhd.uitest;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.test.suitebuilder.annotation.MediumTest;
+
+import com.squareup.spoon.Spoon;
+
+import org.ligi.gobandroid_hd.ui.go_terminology.GoTerminologyViewActivity;
+
+public class TheGoTerminologyActivity extends BaseIntegration<GoTerminologyViewActivity> {
+
+    public TheGoTerminologyActivity() {
+        super(GoTerminologyViewActivity.class);
+    }
+
+    @MediumTest
+    public void testThatGoBoardIsThere() {
+        final Intent intent = new Intent();
+        intent.setData(Uri.parse("goterm://miai"));
+        setActivityIntent(intent);
+        final GoTerminologyViewActivity activity = getActivity();
+
+        Spoon.screenshot(activity, "go_terms");
+    }
+
+
+}
