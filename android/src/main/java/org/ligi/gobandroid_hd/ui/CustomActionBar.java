44f61c7f4ee5832291f1e7470befee677612e231
ligi <ligi@ligi.de>
2014-10-02 22:36:12 +0200

    cleanup

--- android/src/main/java/org/ligi/gobandroid_hd/ui/CustomActionBar.java
+++ android/src/main/java/org/ligi/gobandroid_hd/ui/CustomActionBar.java
@@ -5,8 +5,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
-import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +13,7 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import org.ligi.axt.AXT;
 import org.ligi.axt.listeners.DialogDiscardingOnClickListener;
 import org.ligi.gobandroid_hd.App;
 import org.ligi.gobandroid_hd.InteractionScope;
@@ -24,22 +23,43 @@ import org.ligi.gobandroid_hd.ui.gnugo.GnuGoHelper;
 import org.ligi.gobandroid_hd.ui.ingame_common.SwitchModeHelper;
 import org.ligi.tracedroid.logging.Log;
 
-public class CustomActionBar extends LinearLayout implements
-        GoGame.GoGameChangeListener, DialogInterface {
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
-    private TextView white_captures_tv;
-    private TextView black_captures_tv;
-    private TextView move_tv;
-    private TextView mode_tv;
-    private ViewGroup black_info_container;
-    private ViewGroup white_info_container;
-    private LayoutInflater inflater;
-    private App app;
-    private Activity activity;
+public class CustomActionBar
+        extends LinearLayout
+        implements GoGame.GoGameChangeListener {
 
-    public CustomActionBar(Context ctx, AttributeSet attrs) {
-        super(ctx, attrs);
+    @InjectView(R.id.white_captures_tv)
+    TextView white_captures_tv;
+
+    @InjectView(R.id.black_captures_tv)
+    TextView black_captures_tv;
+
+    @InjectView(R.id.move_tv)
+    TextView move_tv;
+
+    @InjectView(R.id.mode_tv)
+    TextView mode_tv;
+
+    @InjectView(R.id.black_info_container)
+    ViewGroup black_info_container;
+
+    @InjectView(R.id.white_info_container)
+    ViewGroup white_info_container;
+
+    @OnClick(R.id.fake_spinner)
+    void onSpinnerCLick() {
+        showModePopup(activity);
     }
+
+    private final LayoutInflater inflater;
+    private final App app;
+    private final Activity activity;
+
+    private final int highlight_color;
+    private final int transparent;
 
     @Override
     protected void onAttachedToWindow() {
@@ -53,46 +73,28 @@ public class CustomActionBar extends LinearLayout implements
         app.getGame().removeGoGameChangeListener(this);
     }
 
-    public CustomActionBar(Activity _activity) {
-        super(_activity);
+    public CustomActionBar(Activity activity) {
+        super(activity);
 
-        activity = _activity;
-        app = (App) _activity.getApplicationContext();
+        this.activity = activity;
+        app = (App) activity.getApplicationContext();
 
-        inflater = (LayoutInflater) _activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        highlight_color = getResources().getColor(R.color.dividing_color);
+        transparent = getResources().getColor(android.R.color.transparent);
 
-        ViewGroup top_view = (ViewGroup) inflater.inflate(R.layout.top_nav_and_extras, null);
+        inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        white_captures_tv = (TextView) top_view.findViewById(R.id.white_captures_tv);
-        black_captures_tv = (TextView) top_view.findViewById(R.id.black_captures_tv);
-        move_tv = (TextView) top_view.findViewById(R.id.move_tv);
-        mode_tv = (TextView) top_view.findViewById(R.id.mode_tv);
-        black_info_container = (ViewGroup) top_view.findViewById(R.id.black_info_container);
-        white_info_container = (ViewGroup) top_view.findViewById(R.id.white_info_container);
-        // move_tv.setOnClickListener(new ToggleModePopup(context));
+        final ViewGroup top_view = (ViewGroup) inflater.inflate(R.layout.top_nav_and_extras, null);
 
-        top_view.findViewById(R.id.fake_spinner).setOnClickListener(
-                new View.OnClickListener() {
-
-                    @Override
-                    public void onClick(View v) {
-                        showModePopup(v.getContext());
-                    }
-
-                }
-        );
-
-        this.addView(top_view);
-
+        ButterKnife.inject(this, top_view);
+        addView(top_view);
         refresh();
-
     }
 
     private void addItem(LinearLayout container, int image_resId,
-                         int str_resid, final DialogInterface.OnClickListener listener) {
+                         int str_resid, final Runnable listener) {
 
-        Log.i("", "container" + container + " inflater" + inflater);
-        View v = inflater.inflate(R.layout.dropdown_item, null);
+        final View v = inflater.inflate(R.layout.dropdown_item, null);
         ((TextView) v.findViewById(R.id.text)).setText(str_resid);
         ((ImageView) v.findViewById(R.id.image)).setImageResource(image_resId);
 
@@ -101,8 +103,7 @@ public class CustomActionBar extends LinearLayout implements
 
                     @Override
                     public void onClick(View v) {
-                        listener.onClick(CustomActionBar.this, 0);
-
+                        listener.run();
                     }
                 }
         );
@@ -111,35 +112,32 @@ public class CustomActionBar extends LinearLayout implements
 
     private void addModeItem(LinearLayout container, final byte mode,
                              int string_res, int icon_res) {
-        if (mode == app.getInteractionScope().getMode())
-            return; // already here
+        if (mode == app.getInteractionScope().getMode()) {
+            return; // already in this mode - no need to present the user with this option
+        }
 
         addItem(container, icon_res, string_res,
-                new DialogInterface.OnClickListener() {
+                new Runnable() {
 
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
+                    public void run() {
 
                         if (mode == InteractionScope.MODE_GNUGO && !(GnuGoHelper.isGnuGoAvail(activity))) {
                             new AlertDialog.Builder(activity).setTitle(R.string.install_gnugo).setMessage(R.string.gnugo_not_installed)
-                                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                                         @Override
                                         public void onClick(DialogInterface dialog, int which) {
-                                            Intent i = new Intent();
-                                            i.setType(Intent.ACTION_VIEW);
-                                            i.setData(Uri.parse("market://details?id=org.ligi.gobandroidhd.ai.gnugo"));
-                                            activity.startActivity(i);
-
+                                            AXT.at(activity).startCommonIntent().openUrl("market://details?id=org.ligi.gobandroidhd.ai.gnugo");
                                         }
                                     })
-                                    .setNegativeButton("Cancel", new DialogDiscardingOnClickListener())
+                                    .setNegativeButton(android.R.string.cancel, new DialogDiscardingOnClickListener())
                                     .show();
                             return;
                         }
                         activity.finish();
-                        Log.i("settingmode" + mode);
+                        Log.i("set mode" + mode);
                         app.getInteractionScope().setMode(mode);
-                        Intent i = SwitchModeHelper.getIntentByMode(app, mode);
+                        final Intent i = SwitchModeHelper.getIntentByMode(app, mode);
                         activity.startActivity(i);
                     }
 
@@ -148,12 +146,11 @@ public class CustomActionBar extends LinearLayout implements
     }
 
     private void showModePopup(Context ctx) {
-        ScrollView scrollView = new ScrollView(ctx);
-        LinearLayout contentView = new LinearLayout(ctx);
+        final ScrollView scrollView = new ScrollView(ctx);
+        final LinearLayout contentView = new LinearLayout(ctx);
         contentView.setOrientation(LinearLayout.VERTICAL);
-        BitmapDrawableNoMinimumSize background = new BitmapDrawableNoMinimumSize(ctx.getResources(), R.drawable.wood_bg);
+        final BitmapDrawableNoMinimumSize background = new BitmapDrawableNoMinimumSize(ctx.getResources(), R.drawable.wood_bg);
         contentView.setBackgroundDrawable(background);
-
 
         addModeItem(contentView, InteractionScope.MODE_SETUP, R.string.setup, R.drawable.preferences);
 
@@ -179,7 +176,6 @@ public class CustomActionBar extends LinearLayout implements
 
     @Override
     public void onGoGameChange() {
-        Log.i("game changed");
         refresh();
     }
 
@@ -194,25 +190,14 @@ public class CustomActionBar extends LinearLayout implements
                 white_captures_tv.setText("" + app.getGame().getCapturesWhite());
                 black_captures_tv.setText("" + app.getGame().getCapturesBlack());
 
-                int highlight_color = app.getResources().getColor(R.color.dividing_color);
-                int transparent = getResources().getColor(android.R.color.transparent);
-
-                boolean isWhitesMove = app.getGame().isBlackToMove() && (!app.getGame().isFinished());
+                final boolean isWhitesMove = app.getGame().isBlackToMove() && (!app.getGame().isFinished());
                 white_info_container.setBackgroundColor(isWhitesMove ? transparent : highlight_color);
-                boolean isBlacksMove = app.getGame().isBlackToMove() || app.getGame().isFinished();
+                final boolean isBlacksMove = app.getGame().isBlackToMove() || app.getGame().isFinished();
                 black_info_container.setBackgroundColor(isBlacksMove ? highlight_color : transparent);
-                move_tv.setText(app.getResources().getString(R.string.move) + app.getGame().getActMove().getMovePos());
 
+                move_tv.setText(app.getResources().getString(R.string.move) + app.getGame().getActMove().getMovePos());
             }
         });
-    }
-
-    @Override
-    public void cancel() {
-    }
-
-    @Override
-    public void dismiss() {
     }
 
 }
