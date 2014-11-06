61f02db50de6cd2924427698a06d287a8d436430
ligi <ligi@ligi.de>
2014-11-06 13:38:47 +0100

    add tests for scoring - fix review test

--- android/src/androidTest/java/org/ligi/gobandroidhd/uitest/TheReviewActivity.java
+++ android/src/androidTest/java/org/ligi/gobandroidhd/uitest/TheReviewActivity.java
@@ -13,10 +13,7 @@ import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isDisplayed;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isEnabled;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.CoreMatchers.not;
 
 public class TheReviewActivity extends BaseIntegration<GameReviewActivity> {
 
@@ -41,9 +38,7 @@ public class TheReviewActivity extends BaseIntegration<GameReviewActivity> {
         onView(withId(R.id.btn_next)).check(matches(isDisplayed()));
         onView(withId(R.id.btn_prev)).check(matches(isDisplayed()));
         onView(withId(R.id.btn_last)).check(matches(isDisplayed()));
-
         onView(withId(R.id.btn_first)).check(matches(isDisplayed()));
-        onView(withId(R.id.btn_prev)).check(matches(not(isEnabled())));
     }
 
     @MediumTest
@@ -75,7 +70,6 @@ public class TheReviewActivity extends BaseIntegration<GameReviewActivity> {
         assertThat(App.getGame().getActMove().getParent()).isEqualTo(null);
 
     }
-
 
 
 }
