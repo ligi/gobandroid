97befd0562c27013e0b46a7e9c37eaefc64a34c1
ligi <ligi@ligi.de>
2014-11-06 13:42:45 +0100

    add missing import

--- android/src/androidTest/java/org/ligi/gobandroidhd/uitest/TheReviewActivity.java
+++ android/src/androidTest/java/org/ligi/gobandroidhd/uitest/TheReviewActivity.java
@@ -13,6 +13,7 @@ import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isDisplayed;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TheReviewActivity extends BaseIntegration<GameReviewActivity> {
