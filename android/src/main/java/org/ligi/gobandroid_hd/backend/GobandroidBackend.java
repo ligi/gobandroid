a34c8f382477f9993c481b539c1ed1b1ecc55c8a
ligi <ligi@ligi.de>
2015-06-06 21:51:57 +0200

    Fix typo and nice up code

--- android/src/main/java/org/ligi/gobandroid_hd/backend/GobandroidBackend.java
+++ android/src/main/java/org/ligi/gobandroid_hd/backend/GobandroidBackend.java
@@ -2,38 +2,37 @@ package org.ligi.gobandroid_hd.backend;
 
 import android.content.Context;
 import android.provider.Settings.Secure;
-
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLEncoder;
 import org.ligi.axt.AXT;
 import org.ligi.gobandroid_hd.etc.GobandroidConfiguration;
 import org.ligi.tracedroid.logging.Log;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLEncoder;
-
 public class GobandroidBackend {
 
     /**
-     * fetches the nuber of tsumegos available on gogameguru from the server
+     * fetches the number of tsumegos available on gogameguru from the server
      *
      * @return the count or -1 on any error
      */
-    public final static int getMaxTsumegos(Context ctx) {
+    public static int getMaxTsumegos(final Context ctx) {
         try {
-            URL url = new URL("http://" + GobandroidConfiguration.backend_domain + "/tsumegos/max?device_id=" + Secure.getString(ctx.getContentResolver(), Secure.ANDROID_ID));
-            String count_str = AXT.at(url).downloadToString();
+            final URL url = new URL("http://" +
+                                    GobandroidConfiguration.backend_domain +
+                                    "/tsumegos/max?device_id=" +
+                                    Secure.getString(ctx.getContentResolver(), Secure.ANDROID_ID));
+            final String count_str = AXT.at(url).downloadToString();
 
-            count_str = count_str.replace("\n", "").replace("\r", "").trim(); // clean
-            // the
-            // string
-            return Integer.parseInt(count_str);
+            final String cleanedCountStr = count_str.replace("\n", "").replace("\r", "").trim(); // clean the string
+            return Integer.parseInt(cleanedCountStr);
         } catch (Exception e) {
             Log.w("cannot fetch the tsumego count " + e);
             return -1;
         }
     }
 
-    public static String getURLParamSnippet(String key, String val) {
+    public static String getURLParamSnippet(final String key, final String val) {
         try {
             return key + "=" + URLEncoder.encode(val, "UTF-8");
         } catch (UnsupportedEncodingException e) {
