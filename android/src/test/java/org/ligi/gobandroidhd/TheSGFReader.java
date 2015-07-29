4917e3a7a7e9d33f47e06e123151b8c90c7dcdb2
ligi <ligi@ligi.de>
2015-07-30 00:56:53 +0200

    Migrate the SGF read/write tests from instrumentation->unit

--- android/src/test/java/org/ligi/gobandroidhd/TheSGFReader.java
+++ android/src/test/java/org/ligi/gobandroidhd/TheSGFReader.java
@@ -1,59 +1,63 @@
-package org.ligi.gobandroidhd.unittest;
+package org.ligi.gobandroidhd;
 
-import android.test.suitebuilder.annotation.SmallTest;
-
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import org.junit.Test;
+import org.ligi.axt.AXT;
 import org.ligi.gobandroid_hd.logic.GoGame;
 import org.ligi.gobandroid_hd.logic.markers.GoMarker;
 import org.ligi.gobandroid_hd.logic.markers.TextMarker;
 import org.ligi.gobandroid_hd.logic.sgf.SGFReader;
-import org.ligi.gobandroidhd.base.AssetAwareInstrumentationTestCase;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class TheSGFReader extends AssetAwareInstrumentationTestCase {
+public class TheSGFReader {
 
-    @SmallTest
+    private String readAsset(String file) throws IOException, URISyntaxException {
+        return AXT.at(new File(getClass().getClassLoader().getResource(file).toURI())).readToString();
+    }
+    @Test
     public void testReadMinimal19x19SGF() throws Exception {
-        GoGame game = SGFReader.sgf2game(readAsset("sgf/minimal_19x19.sgf"), null);
+        GoGame game = SGFReader.sgf2game(readAsset("test_sgfs/minimal_19x19.sgf"), null);
 
         assertThat(game.getSize()).isEqualTo(19);
     }
 
-    @SmallTest
+    @Test
     public void testReadMinimal9x9SGF() throws Exception {
-        GoGame game = SGFReader.sgf2game(readAsset("sgf/minimal_9x9.sgf"), null);
+        GoGame game = SGFReader.sgf2game(readAsset("test_sgfs/minimal_9x9.sgf"), null);
 
         assertThat(game.getSize()).isEqualTo(9);
     }
 
-    @SmallTest
+    @Test
     public void testReadSmall19x19SGF() throws Exception {
-        GoGame game = SGFReader.sgf2game(readAsset("sgf/small_19x19.sgf"), null);
+        GoGame game = SGFReader.sgf2game(readAsset("test_sgfs/small_19x19.sgf"), null);
 
         assertThat(game.getSize()).isEqualTo(19);
         assertThat(game.getMetaData().getName()).isEqualTo("SMALL19x19");
     }
 
-    @SmallTest
+    @Test
     public void testReadSmall9x9SGF() throws Exception {
-        GoGame game = SGFReader.sgf2game(readAsset("sgf/small_9x9.sgf"), null);
+        GoGame game = SGFReader.sgf2game(readAsset("test_sgfs/small_9x9.sgf"), null);
 
         assertThat(game.getSize()).isEqualTo(9);
         assertThat(game.getMetaData().getName()).isEqualTo("SMALL9x9");
     }
 
 
-    @SmallTest
+    @Test
     // https://github.com/ligi/gobandroid/issues/106
     public void testReadSGFWithFirstMoveWhiteAndCapture() throws Exception {
-        GoGame game = SGFReader.sgf2game(readAsset("sgf/first_move_capture_and_white.sgf"), null);
+        GoGame game = SGFReader.sgf2game(readAsset("test_sgfs/first_move_capture_and_white.sgf"), null);
 
         assertThat(game.getLastMove().getMovePos()).isEqualTo(2);
     }
 
-    @SmallTest
+    @Test
     public void testThatDefaultLabelWorks() throws Exception {
-        GoGame game = SGFReader.sgf2game(readAsset("sgf/default_marker.sgf"), null);
+        GoGame game = SGFReader.sgf2game(readAsset("test_sgfs/default_marker.sgf"), null);
 
         final GoMarker createdMarker = game.getLastMove().getMarkers().get(0);
         assertThat(createdMarker.x).isEqualTo(1);
@@ -61,9 +65,9 @@ public class TheSGFReader extends AssetAwareInstrumentationTestCase {
         assertThat(((TextMarker) createdMarker).getText()).isEqualTo("X");
     }
 
-    @SmallTest
+    @Test
     public void testThatNamedLabelWorks() throws Exception {
-        GoGame game = SGFReader.sgf2game(readAsset("sgf/named_marker.sgf"), null);
+        GoGame game = SGFReader.sgf2game(readAsset("test_sgfs/named_marker.sgf"), null);
 
         final GoMarker createdMarker = game.getLastMove().getMarkers().get(0);
         assertThat(createdMarker.x).isEqualTo(1);
