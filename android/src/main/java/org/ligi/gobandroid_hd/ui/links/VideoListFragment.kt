f9011192425784ad7d5c5be8b3680f5349d96e83
ligi <ligi@ligi.de>
2016-03-11 20:34:33 +0100

    Add video about gobandroid

--- android/src/main/java/org/ligi/gobandroid_hd/ui/links/VideoListFragment.kt
+++ android/src/main/java/org/ligi/gobandroid_hd/ui/links/VideoListFragment.kt
@@ -3,6 +3,7 @@ package org.ligi.gobandroid_hd.ui.links
 class VideoListFragment : LinkListFragment() {
     internal override fun getData(): Array<LinkWithDescription> {
         return arrayOf(
+                LinkWithDescription("https://www.youtube.com/watch?v=TQ82l6TP4O8", "Introduction to Gobandroid", "how to use this app"),
                 LinkWithDescription("https://www.youtube.com/watch?v=irGmbwqqUNs", "About the beauty of go", "surrounding game trailer"),
                 LinkWithDescription("https://www.youtube.com/watch?v=bJILHweVZVw", "Introduction to the rules", "5minute from udacity"),
                 LinkWithDescription("http://www.youtube.com/watch?v=gECcsSeRcNo", "Go tutorial", "3-part tutorial"),
