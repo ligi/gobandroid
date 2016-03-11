fec08fc86de64e56daa440b36be911da2cc6b034
ligi <ligi@ligi.de>
2016-03-11 15:50:54 +0100

    Replace a video

--- android/src/main/java/org/ligi/gobandroid_hd/ui/links/VideoListFragment.kt
+++ android/src/main/java/org/ligi/gobandroid_hd/ui/links/VideoListFragment.kt
@@ -7,8 +7,7 @@ class VideoListFragment : LinkListFragment() {
                 LinkWithDescription("https://www.youtube.com/watch?v=bJILHweVZVw", "Introduction to the rules", "5minute from udacity"),
                 LinkWithDescription("http://www.youtube.com/watch?v=gECcsSeRcNo", "Go tutorial", "3-part tutorial"),
                 LinkWithDescription("https://www.youtube.com/watch?v=nwOZGr-aw9Q", "Japanology Special ", "The game of go"),
-                LinkWithDescription("https://www.youtube.com/watch?v=OUC8AJ-l_vo", "Cinematic ", "some art and beauty"),
-                LinkWithDescription("https://www.youtube.com/watch?v=vFr3K2DORc8", "Deepmind vs Lee Sedol", "Video commentary of historical game")
+                LinkWithDescription("https://www.youtube.com/watch?v=bIQxOsRAXCo", "Deepmind vs Lee Sedol", "Video commentary of historical game")
         )
     }
 }
