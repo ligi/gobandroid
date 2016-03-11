7ff2c41159ea32307fe7243993a16ee6aab72b76
ligi <ligi@ligi.de>
2016-03-11 03:05:51 +0100

    Add 2 more videos

--- android/src/main/java/org/ligi/gobandroid_hd/ui/links/VideoListFragment.kt
+++ android/src/main/java/org/ligi/gobandroid_hd/ui/links/VideoListFragment.kt
@@ -6,6 +6,8 @@ class VideoListFragment : LinkListFragment() {
                 LinkWithDescription("https://www.youtube.com/watch?v=irGmbwqqUNs", "About the beauty of go", "surrounding game trailer"),
                 LinkWithDescription("https://www.youtube.com/watch?v=bJILHweVZVw", "Introduction to the rules", "5minute from udacity"),
                 LinkWithDescription("http://www.youtube.com/watch?v=gECcsSeRcNo", "Go tutorial", "3-part tutorial"),
+                LinkWithDescription("https://www.youtube.com/watch?v=nwOZGr-aw9Q", "Japanology Special ", "The game of go"),
+                LinkWithDescription("https://www.youtube.com/watch?v=OUC8AJ-l_vo", "Cinematic ", "some art and beauty"),
                 LinkWithDescription("https://www.youtube.com/watch?v=vFr3K2DORc8", "Deepmind vs Lee Sedol", "Video commentary of historical game")
         )
     }
