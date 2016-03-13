2ad57d98d413ffc52e7c875c0ba0ce3efe6e1137
ligi <ligi@ligi.de>
2016-03-13 12:38:43 +0100

    Migrate code to Kotlin
    Love these data-classes really boils things down to essentials

---
+++ android/src/main/java/org/ligi/gobandroid_hd/logic/GoGameMetadata.kt
@@ -0,0 +1,36 @@
+/**
+ * gobandroid
+ * by Marcus -Ligi- Bueschleb
+ * http://ligi.de
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation;
+ *
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see //www.gnu.org/licenses/>.
+ */
+
+package org.ligi.gobandroid_hd.logic
+
+/**
+ * store the metadata ( such as player names/ranks) for a game
+ */
+data class GoGameMetadata(var name: String = "",
+                          var result: String = "",
+                          var source: String = "",
+                          var fileName: String = "",
+                          var blackName: String = "",
+                          var blackRank: String = "",
+                          var whiteName: String = "",
+                          var whiteRank: String = "",
+                          var difficulty: String = "",
+                          var date: String = "")
