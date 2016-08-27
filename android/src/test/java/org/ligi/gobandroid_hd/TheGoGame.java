4d86db141e729e4843e2c8a3254e986b01ee6aa5
ligi <ligi@ligi.de>
2016-08-27 18:02:47 +0200

    Cleanup - does not need to extend AssetAwareTest - no assets used

--- android/src/test/java/org/ligi/gobandroid_hd/TheGoGame.java
+++ android/src/test/java/org/ligi/gobandroid_hd/TheGoGame.java
@@ -6,7 +6,7 @@ import org.ligi.gobandroid_hd.logic.GoGame;
 import org.ligi.gobandroid_hd.logic.GoMove;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class TheGoGame extends AssetAwareTest {
+public class TheGoGame {
 
     @Test
     public void testReplayingMoves() {
