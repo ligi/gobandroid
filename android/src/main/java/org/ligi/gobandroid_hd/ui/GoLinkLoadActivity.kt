8ec40edf935621ef82122b48c25fa1a1563d996f
ligi <ligi@ligi.de>
2016-12-09 18:21:41 +0100

    Cleanup and convert to kotlin

---
+++ android/src/main/java/org/ligi/gobandroid_hd/ui/GoLinkLoadActivity.kt
@@ -0,0 +1,45 @@
+package org.ligi.gobandroid_hd.ui
+
+import android.net.Uri
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.ligi.gobandroid_hd.ui.SGFLoadActivity.Companion.INTENT_EXTRA_MOVE_NUM
+import org.ligi.gobandroid_hd.ui.sgf_listing.GoLink
+import org.ligi.gobandroid_hd.ui.sgf_listing.SGFFileSystemListActivity
+import org.ligi.tracedroid.logging.Log
+
+/**
+ * Activity to load a go Link
+ */
+class GoLinkLoadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        GobandroidNotifications(this).cancelGoLinkNotification()
+
+        if (intent.data == null) {
+            Log.e("GoLinkLoadActivity with intent_uri==null")
+            finish()
+            return
+        }
+
+        val link = GoLink(intent.data.toString())
+
+        val intent = intent
+        intent.data = Uri.parse(link.fileName)
+
+        if (link.linksToDirectory()) {
+            intent.setClass(this, SGFFileSystemListActivity::class.java)
+        } else {
+            // we got some sgf - go to sgfload
+            intent.putExtra(INTENT_EXTRA_MOVE_NUM, link.moveDepth)
+            intent.setClass(this, SGFLoadActivity::class.java)
+        }
+
+        startActivity(intent)
+        finish()
+
+    }
+
+}
